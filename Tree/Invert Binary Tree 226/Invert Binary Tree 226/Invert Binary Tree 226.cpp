54b629f64c2bc7c7cdc87caeac9c451989c7b1d6
bekaryss <bekarys.kuralbai@gmail.com>
2020-02-07 23:19:38 +0300

    Daily tasks solving additional

---
+++ Tree/Invert Binary Tree 226/Invert Binary Tree 226/Invert Binary Tree 226.cpp
@@ -0,0 +1,61 @@
+// Invert Binary Tree 226.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* invertTree(TreeNode* root) {
+	if (root == NULL)
+		return NULL;
+	queue<TreeNode*> q;
+	q.push(root);
+	while (!q.empty()) {
+		TreeNode* cur = q.front();
+		q.pop();
+		TreeNode* tmp = cur->left;
+		cur->left = cur->right;
+		cur->right = tmp;
+		if (cur->left != NULL)
+			q.push(cur->left);
+		if (cur->right != NULL)
+			q.push(cur->right);
+	}
+	return root;
+}
+
+TreeNode* invertTreeIterative(TreeNode* root) {
+	if (root == NULL) {
+		return NULL;
+	}
+	TreeNode* left = invertTree(root->left);
+	TreeNode* right = invertTree(root->right);
+	root->left = right;
+	root->right = left;
+	return root;
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
